e8926bda44af0109b9516012b7279c6cff776d54
Stanislav Ovsyankin <sovsyankin@arlo.com>
2022-01-15 12:59:04 +0600

    [FE] Proxy get students request to backend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,11 @@
 import { Button } from "antd";
+import { getAllStudents } from "./client";
 import './App.css';
 
 function App() {
+    getAllStudents()
+        .then(res => res.json())
+        .then(console.log)
   return (
     <div className="App">
       <Button type="primary">Hello</Button>
