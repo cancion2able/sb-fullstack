fdc86649a306a34daa050c5ae422a76989356100
Stanislav Ovsyankin <sovsyankin@arlo.com>
2022-01-21 10:55:04 +0600

    [BE] Make student a database entity

--- src/main/java/com/stnslv/student/Student.java
+++ src/main/java/com/stnslv/student/Student.java
@@ -2,16 +2,24 @@ package com.stnslv.student;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
 
+    @Id
+    @GeneratedValue(generator = "student_sequence", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
     private Long id;
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 }
