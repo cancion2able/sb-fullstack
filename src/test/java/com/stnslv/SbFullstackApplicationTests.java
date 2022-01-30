1da6d3502574647003811dae5495200be9c4ced3
Stanislav Ovsyankin <sovsyankin@arlo.com>
2022-01-30 13:01:24 +0600

    [BE] Add successful test

--- src/test/java/com/stnslv/SbFullstackApplicationTests.java
+++ src/test/java/com/stnslv/SbFullstackApplicationTests.java
@@ -1,6 +1,5 @@
 package com.stnslv;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class SbFullstackApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.fail("Oops test failed");
     }
 
 }
